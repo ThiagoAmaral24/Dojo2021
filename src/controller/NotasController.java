de59a6f03b592d5630f9fad9a1d9f44098ae05fe
ThiagoAmaral24 <48264547+ThiagoAmaral24@users.noreply.github.com>
2021-06-15 20:34:52 -0300

    Initial commit

---
+++ src/controller/NotasController.java
@@ -0,0 +1,23 @@
+package controller;
+
+public class NotasController {
+
+    private int valor;
+    private int qtd_notas_100;
+    private int qtd_nota_50;
+    private int qtd_nota_10;
+    private int	qtd_nota_5;
+
+    private int qtd_moeda_1;
+    private int qtd_moeda_050;
+    private int qtd_moeda_025;
+    private int qtd_moeda_010;
+    private int qtd_moeda_005;
+
+    private Double validacaoNotas(){
+        qtd_notas_100 = valor / 100;
+        valor = valor - (qtd_notas_100 * 100);
+
+        return null;
+    }
+}
