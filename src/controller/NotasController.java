ca9a7363a670481a4d5fa9c6f426ea5b72b90c9b
Erickyoshi <ericshimazu@hotmail.com>
2021-06-15 21:50:31 -0300

    Commit eric

--- src/controller/NotasController.java
+++ src/controller/NotasController.java
@@ -2,7 +2,6 @@ package controller;
 
 public class NotasController {
 
-    private int valor;
     private int qtd_notas_100;
     private int qtd_nota_50;
     private int qtd_nota_10;
@@ -14,10 +13,34 @@ public class NotasController {
     private int qtd_moeda_010;
     private int qtd_moeda_005;
 
-    private Double validacaoNotas(){
+    private String validacaoNotas(int valor){
         int qtd_notas_100 = valor / 100;
         valor -= (qtd_notas_100 * 100);
 
-        return null;
+        int qtd_nota_50 = valor / 50;
+        valor -= (qtd_nota_50 * 50);
+
+        int qtd_nota_10 = valor / 10;
+        valor -= (qtd_nota_10 * 10);
+
+        int qtd_nota_5 = valor / 5;
+        valor -= (qtd_nota_5 * 5);
+
+        int qtd_moeda_1 = valor / 1;
+        valor -= (qtd_moeda_1 * 1);
+
+        int qtd_moeda_050 = valor / (50/100);
+        valor -= (qtd_moeda_050 * (50/100));
+
+        int qtd_moeda_025 = valor / (25/100);
+        valor -= (qtd_moeda_025 * (25/100));
+
+        int qtd_moeda_010 = valor / (10/100);
+        valor -= (qtd_moeda_010 * (10/100));
+
+        int qtd_moeda_005 = valor / (5/100);
+        valor -= (qtd_moeda_005 * (5/100));
+
+        return "";
     }
 }
