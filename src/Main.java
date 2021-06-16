acd49c63bccabf87529c1f6d5d170cce93512475
Erickyoshi <ericshimazu@hotmail.com>
2021-06-15 22:10:21 -0300

    Commit eric2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import controller.NotasController;
+
 public class Main {
     public static void main(String[] args) {
+
+
+        NotasController teste = new NotasController();
+
+        System.out.println(teste.validacaoNotas(576.73));
+
 
     }
 }
--- src/controller/NotasController.java
+++ src/controller/NotasController.java
@@ -4,6 +4,7 @@ public class NotasController {
 
     private int qtd_notas_100;
     private int qtd_nota_50;
+    private int qtd_nota_20;
     private int qtd_nota_10;
     private int	qtd_nota_5;
 
@@ -12,35 +13,55 @@ public class NotasController {
     private int qtd_moeda_025;
     private int qtd_moeda_010;
     private int qtd_moeda_005;
+    private int qtd_moeda_001;
 
-    private String validacaoNotas(int valor){
-        int qtd_notas_100 = valor / 100;
+    public String validacaoNotas(Double valor){
+        Double total = valor;
+        int qtd_notas_100 = (int) (valor / 100);
         valor -= (qtd_notas_100 * 100);
 
-        int qtd_nota_50 = valor / 50;
+        int qtd_nota_50 = (int) (valor / 50);
         valor -= (qtd_nota_50 * 50);
 
-        int qtd_nota_10 = valor / 10;
+        int qtd_nota_20 = (int) (valor / 20);
+        valor -= (qtd_nota_20 * 20);
+
+        int qtd_nota_10 = (int) (valor / 10);
         valor -= (qtd_nota_10 * 10);
 
-        int qtd_nota_5 = valor / 5;
+        int qtd_nota_5 = (int) (valor / 5);
         valor -= (qtd_nota_5 * 5);
 
-        int qtd_moeda_1 = valor / 1;
+        int qtd_moeda_1 = (int) (valor / 1);
         valor -= (qtd_moeda_1 * 1);
 
-        int qtd_moeda_050 = valor / (50/100);
-        valor -= (qtd_moeda_050 * (50/100));
+        int qtd_moeda_050 = (int) (valor / 0.50);
+        valor -= (qtd_moeda_050 * 0.50);
 
-        int qtd_moeda_025 = valor / (25/100);
-        valor -= (qtd_moeda_025 * (25/100));
+        int qtd_moeda_025 = (int) (valor / 0.25);
+        valor -= (qtd_moeda_025 * 0.25);
 
-        int qtd_moeda_010 = valor / (10/100);
-        valor -= (qtd_moeda_010 * (10/100));
+        int qtd_moeda_010 = (int) (valor / 0.10);
+        valor -= (qtd_moeda_010 * 0.10);
 
-        int qtd_moeda_005 = valor / (5/100);
-        valor -= (qtd_moeda_005 * (5/100));
+        int qtd_moeda_005 = (int) (valor / 0.05);
+        valor -= (qtd_moeda_005 * 0.05);
 
-        return "";
+        int qtd_moeda_001 = (int) (valor / 0.01);
+        valor -= (qtd_moeda_001 * 0.01);
+
+        return "Notas 100: " + qtd_notas_100 + "\n" +
+                "Notas 50: " + qtd_nota_50 + "\n" +
+                "Notas 20: " + qtd_nota_20 + "\n" +
+                "Notas 10: " + qtd_nota_10 + "\n" +
+                "Notas 5: " + qtd_nota_5 + "\n" +
+                "Moedas 1: " + qtd_moeda_1 + "\n" +
+                "Moedas 0,50: " + qtd_moeda_050 + "\n" +
+                "Moedas 0,25: " + qtd_moeda_025 + "\n" +
+                "Moedas 0,10: " + qtd_moeda_010 + "\n" +
+                "Moedas 0,05: " + qtd_moeda_005 + "\n" +
+                "Moedas 0,01: " + qtd_moeda_001 + "\n" +
+                "Total: R$" + total;
+
     }
 }
